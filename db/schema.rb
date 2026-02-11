02cd139c158bdfdec6e35e8ff510ac2d6c0421d5
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 <ayushkaushik690@gmail.com>
2026-02-11 15:53:20 +0000

    Production ready configuration

--- db/schema.rb
+++ db/schema.rb
@@ -12,38 +12,38 @@
 
 ActiveRecord::Schema[7.2].define(version: 2026_02_07_094616) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
+  enable_extension "plpgsql"
 
   create_table "papers", force: :cascade do |t|
     t.text "content"
-    t.datetime "created_at", null: false
     t.string "paper_type"
-    t.integer "rotation"
-    t.datetime "updated_at", null: false
-    t.bigint "wall_id", null: false
     t.integer "x"
     t.integer "y"
+    t.integer "rotation"
     t.integer "z_index"
+    t.bigint "wall_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["wall_id"], name: "index_papers_on_wall_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "walls", force: :cascade do |t|
-    t.datetime "created_at", null: false
     t.string "title"
-    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_walls_on_user_id"
   end
 
